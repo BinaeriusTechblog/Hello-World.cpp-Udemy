8a4218ea0de9b8e1334493ff5a91370398d7e4f7
BinaeriusTechblog <45320679+BinaeriusTechblog@users.noreply.github.com>
2019-07-11 03:40:24 +0200

    initial commit

---
+++ CodeKursCpp.cpp
@@ -0,0 +1,53 @@
+/*
+Der Befehl "include" teilt dem Debugger mit dass etwas inkludiert werden soll
+
+Bei "iostream" handelt es sich um eine Standardfunktionsbibliothek
+*/
+#include <iostream>
+
+
+//Es wird ein Namensraum deklariert um uns die Arbeit zu erleichtern es ist jedoch nicht zwingend erforderlich
+using namespace std;
+
+//"int main()" ist unser Funktionskopf, während sich der Funktionsrumpf in geschweiften Klammern befindet
+int main()
+{
+	/* 
+	Mit dem Befeh " cout<< " gibt man Text aus, der Text den man ausgeben möchte befindet sich zwischen Gänsefüßchen 
+
+	Mit dem Befehl " <<endl " könnt ihr einen Zeilenumbruch bewirken
+
+	Eine jede Zeile muss mit einem Semikolon beendet werden
+
+	Deutsche Ausgabe
+	*/
+	cout << "Hallo Deutschland"<<endl;
+	/*
+	Englische 
+	Text
+	*/
+	std::cout << "Hello  Germany"<<endl;
+
+	cout << "Hallo Europa" << endl;
+	/*
+	Englische
+	Text
+	*/
+	std::cout << "Hello  Europe" << endl;
+	
+	cout << "Hallo Welt " << endl;
+	/*
+	Englische
+	Text
+	*/
+	std::cout << "Hello  World " << endl;
+
+	cout << "Hallo Galaxie" << endl;
+	/*
+	Englische
+	Text
+	*/
+	std::cout << "Hello  Galaxy" << endl;
+
+
+}
