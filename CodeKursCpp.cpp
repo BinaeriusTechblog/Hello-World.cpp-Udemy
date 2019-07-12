c037c8231a3c249f570528ccbcd8182e7588683c
BinaeriusTechblog <45320679+BinaeriusTechblog@users.noreply.github.com>
2019-07-12 23:12:43 +0200

    updatet

--- CodeKursCpp.cpp
+++ CodeKursCpp.cpp
@@ -9,13 +9,13 @@ Bei "iostream" handelt es sich um eine Standardfunktionsbibliothek
 //Es wird ein Namensraum deklariert um uns die Arbeit zu erleichtern es ist jedoch nicht zwingend erforderlich
 using namespace std;
 
-//"int main()" ist unser Funktionskopf, w‰hrend sich der Funktionsrumpf in geschweiften Klammern befindet
+//"int main()" ist unser Funktionskopf, w√§hrend sich der Funktionsrumpf in geschweiften Klammern befindet
 int main()
 {
 	/* 
-	Mit dem Befeh " cout<< " gibt man Text aus, der Text den man ausgeben mˆchte befindet sich zwischen G‰nsef¸ﬂchen 
+	Mit dem Befeh " cout<< " gibt man Text aus, der Text den man ausgeben m√∂chte befindet sich zwischen G√§nsef√º√üchen 
 
-	Mit dem Befehl " <<endl " kˆnnt ihr einen Zeilenumbruch bewirken
+	Mit dem Befehl " <<endl " k√∂nnt ihr einen Zeilenumbruch bewirken
 
 	Eine jede Zeile muss mit einem Semikolon beendet werden
 
@@ -26,28 +26,29 @@ int main()
 	Englische 
 	Text
 	*/
-	std::cout << "Hello  Germany"<<endl;
+	cout << "Hello  Germany"<<endl;
 
 	cout << "Hallo Europa" << endl;
 	/*
 	Englische
 	Text
 	*/
-	std::cout << "Hello  Europe" << endl;
+	cout << "Hello  Europe" << endl;
 	
 	cout << "Hallo Welt " << endl;
 	/*
 	Englische
 	Text
 	*/
-	std::cout << "Hello  World " << endl;
+	cout << "Hello  World " << endl;
 
 	cout << "Hallo Galaxie" << endl;
 	/*
 	Englische
 	Text
 	*/
-	std::cout << "Hello  Galaxy" << endl;
+	cout << "Hello  Galaxy" << endl;
 
+	return 0;
 
 }
